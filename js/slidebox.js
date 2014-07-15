8754ead0e0490191f6666781c6b664f270bf5482
Gustavo Robles <gustavo.robles@crowdint.com>
2014-07-15 12:33:17 -0500

    add slide page indicators

--- js/slidebox.js
+++ js/slidebox.js
@@ -2,7 +2,11 @@
  * Slidebox Directive
  *
  * version 0.9
- * http://github.com/keithjgrant/slidebox
+ * http://github.com/gus4no/slidebox
+ *
+ * by Gustavo Robles
+ *
+ * forked from http://github.com/keithjgrant/slidebox
  *
  * by Keith J Grant
  * http://elucidblue.com
@@ -15,6 +19,7 @@ angular.module('Slidebox', [])
             '<div class="slidebox">' +
             '<div ng-transclude class="slidebox-content"></div>' +
             '</div>' +
+            '<ol class="slide-indicators"></ol>' +
             '<div class="slidebox-controls slidebox-left"></div>' +
             '<div class="slidebox-controls slidebox-right"></div>' +
             '</div>',
@@ -31,28 +36,35 @@ angular.module('Slidebox', [])
           });
 
           $scope.$parent.$watch('vm.finishedRender', function(newValue, oldValue){
-            if(!_.isUndefined(newValue)){
+            if(typeof(newValue) != 'undefined'){
               setItemsWidth(itemWidth);
+              setIndicators(true);
             }
           });
 
           var content = element.children()[0],
-              leftEl = element.children()[1],
-              rightEl = element.children()[2],
+              list = element.children()[1],
+              leftEl = element.children()[2],
+              rightEl = element.children()[3],
               perPageDesktop = Number(attrs.perPageDesktop) || 4,
               perPageTablet = Number(attrs.perPageTablet) || 3,
               perPagePhone = Number(attrs.perPagePhone) || 1,
               itemWidth = (content.clientWidth - 20) / getPageSize(),
               items = content.children[0].children,
-              interval, length;
+              interval, length, lastActiveSlidePosition;
 
-
+          var hammer = Hammer(content, { touchAction: 'pan-y'});
+          hammer.on('swipeleft', scrollRight);
+          hammer.on('swiperight', scrollLeft);
           rightEl.addEventListener('click', scrollRight);
           leftEl.addEventListener('click', scrollLeft);
           window.addEventListener('orientationchange',recalculateWidths);
+          window.onresize = recalculateWidths;
+
 
           function scrollLeft(){
             var limit = getScrollLimit(true);
+            setActiveSlide(-1);
             interval = setInterval(function () {
               content.scrollLeft -= 10;
               if(content.scrollLeft <= limit || content.scrollLeft <= 0){
@@ -63,6 +75,7 @@ angular.module('Slidebox', [])
 
           function scrollRight(){
             var limit = getScrollLimit();
+            setActiveSlide(1);
             var maxScroll = content.scrollWidth - content.clientWidth;
             interval = setInterval(function () {
               content.scrollLeft += 10;
@@ -71,7 +84,6 @@ angular.module('Slidebox', [])
               }
             }, 1);
           }
-
 
           function setItemsWidth(width){
             for(var i = 0; i < items.length; i++){
@@ -86,19 +98,20 @@ angular.module('Slidebox', [])
             itemWidth = content.clientWidth / getPageSize();
             content.children[0].style.width = itemWidth * length + 20 + 'px';
             setItemsWidth(itemWidth);
+            setIndicators();
           }
 
           function getPageSize(){
             var width = window.innerWidth;
             switch (true) {
+              case(width > 980):
+                return perPageDesktop;
+                break;
               case (width < 980 && width > 769):
                 return perPageTablet;
                 break;
               case (width < 768):
                 return perPagePhone;
-                break;
-              default:
-                return perPageDesktop;
                 break;
             }
           }
@@ -114,11 +127,33 @@ angular.module('Slidebox', [])
             }else{
               return content.scrollLeft + containerWidth
             }
-
           }
 
-          window.onresize = recalculateWidths;
+          function setIndicators(firstTime){
+            var indicators = Math.ceil(items.length / getPageSize());
+            list.innerText = '';
+            for(var i = 0; i < indicators; i++){
+              indicator = document.createElement('li');
+              indicator.setAttribute('position', i);
+              indicator.className = 'indicator';
+              list.appendChild(indicator);
+            }
 
+            if(firstTime){ lastActiveSlidePosition = 0; }
+            list.querySelectorAll("[position='"+ lastActiveSlidePosition +"']")[0].className += ' active';
+          }
+
+          function setActiveSlide(transition){
+            var active = list.getElementsByClassName('active')[0]
+            var position = Number(active.getAttribute('position')) + transition;
+            var newActive = list.querySelectorAll("[position='"+ position +"']")[0];
+
+            if(typeof(newActive) != 'undefined'){
+              lastActiveSlidePosition = position;
+              newActive.className += ' active';
+              active.className = 'indicator';
+            }
+          }
         }
       };
     })
